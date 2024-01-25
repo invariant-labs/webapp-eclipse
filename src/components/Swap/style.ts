1a591637abe72980066383881144af7bc1e4e63e
zielvna <zielvna@gmail.com>
2024-01-25 13:16:29 +0100

    add refresh button

--- src/components/Swap/style.ts
+++ src/components/Swap/style.ts
@@ -66,6 +66,15 @@ const useStyles = makeStyles((theme: Theme) => ({
       color: colors.white.main
     }
   },
+  refreshIcon: {
+    width: 26,
+    height: 21,
+    cursor: 'pointer',
+    transition: 'filter 100ms',
+    '&:hover': {
+      filter: 'brightness(1.5)'
+    }
+  },
   settingsIcon: {
     width: 20,
     height: 20,
@@ -94,6 +103,20 @@ const useStyles = makeStyles((theme: Theme) => ({
     color: colors.invariant.lightHover
   },
 
+  swapControls: {
+    display: 'flex',
+    gap: 8
+  },
+
+  refreshIconBtn: {
+    padding: 0,
+    margin: 0,
+    minWidth: 'auto',
+    background: 'none',
+    '& :hover': {
+      background: 'none'
+    }
+  },
   settingsIconBtn: {
     padding: 0,
     margin: 0,
