18c6d47a24276ff56ec6a7521a6d2ac6981356a9
Jakub Wołodko <kuba8369@gmail.com>
2024-10-08 22:43:41 +0200

    Update

--- src/pages/SolanaCreator/SolanaCreator.tsx
+++ src/pages/SolanaCreator/SolanaCreator.tsx
@@ -1,20 +1,13 @@
-import React, { useState } from 'react'
-// import WrappedSwap from '@containers/WrappedSwap/WrappedSwap'
+import React from 'react'
 import useStyles from './styles'
 import { Grid } from '@material-ui/core'
 import { CreateToken } from './components/CreateToken/CreateToken'
 
 export const SolanaCreator: React.FC = () => {
   const classes = useStyles()
-  // const [value, setValue] = useState('')
-  // const searchFarm = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setValue(e.target.value.toLowerCase())
-  // }
   return (
     <Grid container className={classes.container}>
       <CreateToken />
-      {/* <SearchInput handleChange={searchFarm} value={value} /> */}
-      {/* <WrappedSwap /> */}
     </Grid>
   )
 }
