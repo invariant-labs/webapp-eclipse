31a7eee63bf737cc4eb16810f78410c2411a608a
zielvna <zielvna@gmail.com>
2024-10-16 14:09:14 +0200

    remove creator from menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -65,7 +65,7 @@ export const Header: React.FC<IHeader> = ({
 
   const isMdDown = useMediaQuery(theme.breakpoints.down('md'))
 
-  const routes = ['exchange', 'liquidity', 'statistics', 'creator']
+  const routes = ['exchange', 'liquidity', 'statistics' /*, 'creator'*/]
 
   const otherRoutesToHighlight: Record<string, RegExp[]> = {
     liquidity: [/^newPosition\/*/, /^position\/*/],
