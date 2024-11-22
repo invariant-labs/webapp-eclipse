880878e9679eb18bc0e402f6a9d614abc36f334d
Piotr Matlak <piotr.matlak.contact@gmail.com>
2024-11-22 10:05:51 +0100

    trunk format

--- src/components/NewPosition/DepositSelector/DepositSelector.tsx
+++ src/components/NewPosition/DepositSelector/DepositSelector.tsx
@@ -395,7 +395,9 @@ export const DepositSelector: React.FC<IDepositSelector> = ({
           tokenPrice={priceA}
           currency={tokenAIndex !== null ? tokens[tokenAIndex].symbol : null}
           currencyIconSrc={tokenAIndex !== null ? tokens[tokenAIndex].logoURI : undefined}
-          currencyIsUnknown={tokenAIndex !== null ? tokens[tokenAIndex].isUnknown ?? false : false}
+          currencyIsUnknown={
+            tokenAIndex !== null ? (tokens[tokenAIndex].isUnknown ?? false) : false
+          }
           placeholder='0.0'
           onMaxClick={() => {
             if (tokenAIndex === null) {
@@ -450,7 +452,9 @@ export const DepositSelector: React.FC<IDepositSelector> = ({
           tokenPrice={priceB}
           currency={tokenBIndex !== null ? tokens[tokenBIndex].symbol : null}
           currencyIconSrc={tokenBIndex !== null ? tokens[tokenBIndex].logoURI : undefined}
-          currencyIsUnknown={tokenBIndex !== null ? tokens[tokenBIndex].isUnknown ?? false : false}
+          currencyIsUnknown={
+            tokenBIndex !== null ? (tokens[tokenBIndex].isUnknown ?? false) : false
+          }
           placeholder='0.0'
           onMaxClick={() => {
             if (tokenBIndex === null) {
