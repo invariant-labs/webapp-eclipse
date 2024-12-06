305012850930c9def31815c5a59de0f92db38a93
Jakub Wołodko <kuba8369@gmail.com>
2024-12-06 22:12:43 +0100

    remove unused imports

--- src/pages/LeaderboardPage/components/Faq/Faq.tsx
+++ src/pages/LeaderboardPage/components/Faq/Faq.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Faq = () => {
   return <div>Faq</div>
 }
--- src/pages/LeaderboardPage/components/ItemList/ItemList.tsx
+++ src/pages/LeaderboardPage/components/ItemList/ItemList.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useStyles } from './style'
 import { Grid } from '@mui/material'
 import Item from '../Item/Item'
