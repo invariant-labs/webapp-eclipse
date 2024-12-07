ee22810e0e1a2d0bdcd701f7c3e45f3f354faa08
Jakub Wołodko <kuba8369@gmail.com>
2024-12-07 20:21:32 +0100

    remove unused usestate

--- src/pages/LeaderboardPage/components/ItemList/ItemList.tsx
+++ src/pages/LeaderboardPage/components/ItemList/ItemList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useStyles } from './style'
 import { Grid } from '@mui/material'
 import Item from '../Item/Item'
