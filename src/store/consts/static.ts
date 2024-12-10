b0e31e74bbbae2a946b784ac30911a862ddcbe1a
zielvna <zielvna@gmail.com>
2024-12-10 19:06:36 +0100

    add pool apy to stats

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -675,9 +675,9 @@ export const enum SortTypePoolList {
   VOLUME_ASC,
   VOLUME_DESC,
   TVL_ASC,
-  TVL_DESC
-  // APY_ASC,
-  // APY_DESC
+  TVL_DESC,
+  APY_ASC,
+  APY_DESC
 }
 
 export const enum SortTypeTokenList {
