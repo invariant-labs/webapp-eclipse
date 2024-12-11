d66c766eb274162f8174eaa7c3979d5cff5c0c17
zielvna <zielvna@gmail.com>
2024-12-11 10:28:29 +0100

    change background color of header

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -11,6 +11,7 @@ const useStyles = makeStyles()((theme: Theme) => {
       height: 70,
       display: 'flex',
       justifyContent: 'space-between',
+      background: colors.invariant.component,
 
       [theme.breakpoints.down('xl')]: {
         paddingInline: 32
@@ -89,7 +90,7 @@ const useStyles = makeStyles()((theme: Theme) => {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: colors.invariant.component,
+      backgroundColor: colors.invariant.light,
       borderRadius: 14,
       width: 'auto',
       padding: 4
