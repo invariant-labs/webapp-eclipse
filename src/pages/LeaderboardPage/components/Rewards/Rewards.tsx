c578468369a0ac88ddc38af420f369f0dd5ddcb1
Jakub Wołodko <kuba8369@gmail.com>
2024-12-15 17:05:34 +0100

    redesign rewards

--- src/pages/LeaderboardPage/components/Rewards/Rewards.tsx
+++ src/pages/LeaderboardPage/components/Rewards/Rewards.tsx
@@ -22,39 +22,39 @@ export const Rewards = () => {
     <Box className={classes.infoContainer}>
       <Box style={{ width: 'auto' }}>
         <Typography className={classes.header}>Rewards</Typography>
-        {isConnected ? (
-          <>
-            <Typography className={classes.description}>
-              Invariant Points are a rewards program designed to incentivize liquidity providers on
-              Invariant. Earn points by providing liquidity and participating in community
-              activities. Develop your own liquidity provision strategy and climb to the top of the
-              leaderboard. Accumulated points can be used for future exclusive benefits.
-            </Typography>
-            <Box style={{ marginTop: '64px' }}>
-              <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <img src={icons.airdrop} style={{ height: '48px', marginRight: '24px' }} />
-                <Typography
-                  style={{
-                    color: colors.invariant.textGrey,
-                    fontSize: '32px',
-                    fontWeight: 400,
-                    lineHeight: '36px',
-                    letterSpacing: '-3%'
-                  }}>
-                  You have now:
-                </Typography>
-              </Box>
-              <Box
+        <>
+          <Typography className={classes.description}>
+            Invariant Points are a rewards program designed to incentivize liquidity providers on
+            Invariant. Earn points by providing liquidity and participating in community activities.
+            Develop your own liquidity provision strategy and climb to the top of the leaderboard.
+            Accumulated points can be used for future exclusive benefits.
+          </Typography>
+          <Box style={{ marginTop: '64px' }}>
+            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <img src={icons.airdrop} style={{ height: '48px', marginRight: '24px' }} />
+              <Typography
                 style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  flexDirection: 'column',
-                  alignItems: 'center'
+                  color: colors.invariant.textGrey,
+                  fontSize: '32px',
+                  fontWeight: 400,
+                  lineHeight: '36px',
+                  letterSpacing: '-3%'
                 }}>
-                <Typography className={classes.pointsValue}>
-                  {currentUser?.points ?? 0} points
-                </Typography>
-                <Box style={{ width: '250px' }}>
+                You have now:
+              </Typography>
+            </Box>
+            <Box
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}>
+              <Typography className={classes.pointsValue}>
+                {currentUser?.points ?? 0} points
+              </Typography>
+              <Box style={{ width: '250px' }}>
+                {isConnected ? (
                   <AnimatedButton
                     disabled={true}
                     progress={'none'}
@@ -62,61 +62,35 @@ export const Rewards = () => {
                     content='Claim'
                     onClick={() => {}}
                   />
-                </Box>
+                ) : (
+                  <ChangeWalletButton
+                    name='Connect wallet'
+                    onConnect={() => {
+                      dispatch(walletActions.connect(false))
+                    }}
+                    connected={false}
+                    onDisconnect={() => {}}
+                    className={classes.connectWalletButton}
+                  />
+                )}
               </Box>
             </Box>
-            <Typography
-              className={classes.description}
-              style={{
-                marginTop: '64px',
-                display: 'flex',
-                justifyItems: 'center',
-                alignItems: 'center',
-                flexDirection: 'column'
-              }}>
-              If you want to learn more about point system, read about them in our Docs.
-              <Button className={classes.button} style={{ marginTop: '32px' }}>
-                Learn More <LaunchIcon classes={{ root: classes.clipboardIcon }} />
-              </Button>
-            </Typography>
-          </>
-        ) : (
-          <>
-            <Typography
-              style={{
-                color: colors.invariant.textGrey,
-                fontSize: '32px',
-                textAlign: 'center',
-                letterSpacing: '-3%',
-                lineHeight: '36px',
-                fontWeight: 400,
-                marginTop: '32px'
-              }}>
-              Find out your eligibility by connecting your Wallet below!
-            </Typography>
-            <Typography
-              style={{
-                color: colors.invariant.textGrey,
-                fontSize: '24px',
-                textAlign: 'center',
-                letterSpacing: '-3%',
-                lineHeight: '28px',
-                fontWeight: 400,
-                marginTop: '32px'
-              }}>
-              You need to connect you Wallet before proceeding to claim your Airdrop!
-            </Typography>
-            <ChangeWalletButton
-              name='Connect wallet'
-              onConnect={() => {
-                dispatch(walletActions.connect(false))
-              }}
-              connected={false}
-              onDisconnect={() => {}}
-              className={classes.connectWalletButton}
-            />
-          </>
-        )}
+          </Box>
+          <Typography
+            className={classes.description}
+            style={{
+              marginTop: '64px',
+              display: 'flex',
+              justifyItems: 'center',
+              alignItems: 'center',
+              flexDirection: 'column'
+            }}>
+            If you want to learn more about point system, read about them in our Docs.
+            <Button className={classes.button} style={{ marginTop: '32px' }}>
+              Learn More <LaunchIcon classes={{ root: classes.clipboardIcon }} />
+            </Button>
+          </Typography>
+        </>
       </Box>
     </Box>
   )
