fe7fdabbaa54411d9814d59cd32bcd2e85a101bc
Jakub Wołodko <kuba8369@gmail.com>
2024-12-18 22:24:58 +0100

    fixes

--- src/pages/LeaderboardPage/components/YourProgress/YourProgress.tsx
+++ src/pages/LeaderboardPage/components/YourProgress/YourProgress.tsx
@@ -16,6 +16,9 @@ import { useSelector } from 'react-redux'
 import { BlurOverlay } from './BlurOverlay'
 import { ProgressItem } from './ProgressItem'
 import { leaderboardSelectors } from '@store/selectors/leaderboard'
+import { BN } from '@coral-xyz/anchor'
+import { LEADERBOARD_DECIMAL } from '@pages/LeaderboardPage/config'
+import { printBN } from '@utils/utils'
 
 interface YourProgressProps {
   userStats: UserStats | null
@@ -49,7 +52,7 @@ export const YourProgress: React.FC<YourProgressProps> = ({ userStats }) => {
           tooltip='Points refresh every hour.'
           desktopLabelAligment='right'
           label='Total points'
-          value={userStats?.points ?? 0}
+          value={printBN(new BN(userStats?.points, 'hex') ?? 0, LEADERBOARD_DECIMAL)}
         />
         <ProgressItem
           background={{
