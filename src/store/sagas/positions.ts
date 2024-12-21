3fb5a853526a1783c57ea19dfab3b70ed1d7f931
Wojciech <cichockiwojciech97@gmail.com>
2024-12-21 10:20:46 +0100

    Fix unused variables

--- src/store/sagas/positions.ts
+++ src/store/sagas/positions.ts
@@ -355,9 +355,7 @@ function* handleInitPositionWithETH(action: PayloadAction<InitPositionData>): Ge
     const tokensAccounts = yield* select(accounts)
     const allTokens = yield* select(tokens)
     const allPools = yield* select(poolsArraySortedByFees)
-    // TODO: uncomment after fix
-    /* tslint:disable:no-unused-variable */
-    const ticks = yield* select(plotTicks)
+    // const ticks = yield* select(plotTicks)
 
     const pair = new Pair(data.tokenX, data.tokenY, {
       fee: data.fee,
