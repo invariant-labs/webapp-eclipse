4d98147cf321a150d1580021699151977ed9dd14
matepal00 <mateusz.paluch2311@gmail.com>
2025-01-18 10:52:01 +0100

    fixx dev top scorers

--- src/pages/LeaderboardPage/components/TopScorers/TopScorers.tsx
+++ src/pages/LeaderboardPage/components/TopScorers/TopScorers.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import useStyles from './styles'
 import { LeaderboardEntry } from '@store/reducers/leaderboard'
 import { ScorerItem } from './ScorerItem'
+import { BN } from '@coral-xyz/anchor'
 
 interface ITopScorersProps {
   top3Scorers: LeaderboardEntry[]
@@ -11,36 +12,37 @@ interface ITopScorersProps {
 export const TopScorers: React.FC<ITopScorersProps> = ({ top3Scorers }) => {
   const { classes } = useStyles()
   const [firstPlace, secondPlace, thirdPlace] = top3Scorers
+  console.log(firstPlace, secondPlace, thirdPlace)
   return (
     <>
       <Typography className={classes.leaderboardHeaderSectionTitle}>Top Scorers</Typography>
       <Box className={classes.sectionContent}>
         <Hidden lgUp>
           <ScorerItem
-            points={firstPlace?.points ?? null}
+            points={new BN(firstPlace?.points, 'hex') ?? null}
             cupVariant='gold'
             address={firstPlace?.address ?? null}
             showPlaceholder={!firstPlace || !secondPlace || !thirdPlace}
           />
         </Hidden>
         <ScorerItem
-          points={firstPlace?.points ?? null}
+          points={new BN(secondPlace?.points, 'hex') ?? null}
           cupVariant='silver'
-          address={firstPlace?.address ?? null}
+          address={secondPlace?.address ?? null}
           showPlaceholder={!firstPlace || !secondPlace || !thirdPlace}
         />
         <Hidden lgDown>
           <ScorerItem
-            points={firstPlace?.points ?? null}
+            points={new BN(firstPlace?.points, 'hex') ?? null}
             cupVariant='gold'
             address={firstPlace?.address ?? null}
             showPlaceholder={!firstPlace || !secondPlace || !thirdPlace}
           />
         </Hidden>
         <ScorerItem
-          points={firstPlace?.points ?? null}
+          points={new BN(thirdPlace?.points, 'hex') ?? null}
           cupVariant='bronze'
-          address={firstPlace?.address ?? null}
+          address={thirdPlace?.address ?? null}
           showPlaceholder={!firstPlace || !secondPlace || !thirdPlace}
         />
       </Box>
