cb97da1c87458b506f67b0181b41c0ed0750a6a3
Piotr Matlak <piotr.matlak.contact@gmail.com>
2025-01-20 15:58:01 +0100

    update price impact warning

--- src/components/Swap/Swap.tsx
+++ src/components/Swap/Swap.tsx
@@ -825,7 +825,7 @@ export const Swap: React.FC<ISwap> = ({
         <Box
           className={classes.unknownWarningContainer}
           style={{ height: IS_ERROR_LABEL_SHOW ? '34px' : '0px' }}>
-          {+printBN(simulateResult.priceImpact, DECIMAL - 2) > 25 && (
+          {+printBN(simulateResult.priceImpact, DECIMAL - 2) > 10 && (
             <TooltipHover text='Your trade size might be too large'>
               <Box className={classes.unknownWarning}>
                 {(+printBN(simulateResult.priceImpact, DECIMAL - 2)).toFixed(2)}% Price impact
