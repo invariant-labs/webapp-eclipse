e556ac866646358282fb271bb7aad2f96f3aa1d5
Jakub Wołodko <kuba8369@gmail.com>
2025-01-21 18:24:20 +0100

    Added animation

--- src/components/Swap/EstimatedPointsLabel/EstimatedPointsLabel.tsx
+++ src/components/Swap/EstimatedPointsLabel/EstimatedPointsLabel.tsx
@@ -25,23 +25,40 @@ export const EstimatedPointsLabel: React.FC<IEstimatedPointsLabel> = ({
   stringPointsValue
 }) => {
   const { classes } = useStyles({ isVisible: isAnimating })
+
   return (
     <Box
       className={classes.pointsBox}
       ref={pointsBoxRef}
       onPointerLeave={handlePointerLeave}
       onPointerEnter={handlePointerEnter}>
-      <img src={icons.airdropRainbow} alt='' />
-      Points{' '}
-      {new BN(swapMultiplier, 'hex').gte(new BN(1)) &&
-        `${new BN(swapMultiplier, 'hex').toNumber()}x`}
-      :{' '}
-      <span className={classes.pointsAmount}>
-        {formatNumber(
-          removeAdditionalDecimals(stringPointsValue, isLessThanOne ? decimalIndex : 2)
-        )}
-      </span>{' '}
-      <img src={icons.infoCircle} alt='' width={'14px'} style={{ marginTop: '-2px' }} />
+      <div className={classes.contentWrapper}>
+        <img src={icons.airdropRainbow} alt='' />
+        Points{' '}
+        {new BN(swapMultiplier, 'hex').gte(new BN(1)) &&
+          `${new BN(swapMultiplier, 'hex').toNumber()}x`}
+        :{' '}
+        <span className={classes.pointsAmount}>
+          {formatNumber(
+            removeAdditionalDecimals(stringPointsValue, isLessThanOne ? decimalIndex : 2)
+          )}
+        </span>{' '}
+        <img src={icons.infoCircle} alt='' width='14px' style={{ marginTop: '-2px' }} />
+      </div>
+
+      <div className={classes.alternativeContent}>
+        <img src={icons.airdropRainbow} alt='' className={classes.grayscaleIcon} />
+        How to earn points?
+        <img
+          src={icons.infoCircle}
+          alt=''
+          width='14px'
+          style={{ marginTop: '-2px' }}
+          className={classes.grayscaleIcon}
+        />
+      </div>
     </Box>
   )
 }
+
+export default EstimatedPointsLabel
--- src/components/Swap/EstimatedPointsLabel/style.ts
+++ src/components/Swap/EstimatedPointsLabel/style.ts
@@ -4,23 +4,63 @@ import { makeStyles } from 'tss-react/mui'
 const useStyles = makeStyles<{ isVisible: boolean }>()((_theme, { isVisible }) => ({
   pointsBox: {
     height: 27,
+    minWidth: isVisible ? '110px' : '160px',
     padding: '0px 8px',
     borderRadius: 8,
     backgroundColor: colors.invariant.component,
     color: colors.invariant.textGrey,
     fontSize: 16,
-    fontsWeigth: 500,
-    opacity: isVisible ? 1 : 0,
+    fontWeight: 500,
     cursor: 'pointer',
     userSelect: 'none',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     textTransform: 'none',
+    willChange: 'width',
     gap: 4,
-    transition: 'all 0.7s ease-in-out'
+    transition: isVisible ? 'all 0.7s ease-in-out' : 'min-width 0.7s ease-in-out',
+    position: 'relative',
+    overflow: 'hidden'
   },
-  pointsAmount: { color: colors.invariant.pink }
+
+  pointsAmount: {
+    color: colors.invariant.pink
+  },
+  contentWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4,
+    width: '100%',
+    left: 8,
+    position: 'absolute',
+    willChange: 'opacity, transform',
+    opacity: isVisible ? 1 : 0,
+    transform: `translateY(${isVisible ? '0' : '-100%'})`,
+    transition: isVisible
+      ? 'opacity 0.7s ease-in-out, transform 0.5s ease-in-out'
+      : 'opacity 0.7s ease-in-out 0.7s, transform 0.5s ease-in-out 0.7s'
+  },
+  alternativeContent: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4,
+    width: '100%',
+    left: 8,
+    position: 'absolute',
+    willChange: 'opacity, transform',
+    opacity: isVisible ? 0 : 1,
+    transform: `translateY(${isVisible ? '100%' : '0'})`,
+    transition: isVisible
+      ? 'opacity 0.7s ease-in-out, transform 0.5s ease-in-out'
+      : 'opacity 0.7s ease-in-out 0.7s, transform 0.5s ease-in-out 0.7s'
+  },
+  grayscaleIcon: {
+    filter: 'grayscale(100%)',
+    transition: 'filter 0.7s ease-in-out',
+    minWidth: '14px',
+    minHeight: '14px'
+  }
 }))
 
 export default useStyles
