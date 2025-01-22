2a2e6205cda121554a6aa6f3a7133c36244f081a
Jakub Wołodko <kuba8369@gmail.com>
2025-01-22 08:49:58 +0100

    Clean

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1723,10 +1723,6 @@ export const getJupTokenPrice = async (solanaAddress: string): Promise<number | 
 }
 
 export const getTokenPrice = async (id: string): Promise<number | undefined> => {
-  if (id === 'ethereum') {
-    id = 'bridged-wrapped-ether-eclipse'
-  }
-
   const token = TOKENS_PRICES_FROM_JUP.find(token => token.coingeckoId === id)
   if (token && token.solanaAddress) {
     return await getJupTokenPrice(token.solanaAddress)
