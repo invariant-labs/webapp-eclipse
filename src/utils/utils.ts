9cad61bbfb7d4f30d9e8ed4ce9a45985cb40220f
Jakub Wołodko <kuba8369@gmail.com>
2025-01-23 19:26:48 +0100

    Remove duplicated

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -726,26 +726,6 @@ export const printSubNumber = (amount: number): string => {
     .join('')
 }
 
-export const FormatConfig = {
-  Q: 1000000000000000000, // 10^18
-  T: 1000000000000000, // 10^15
-  H: 100000000000000, // 10^14 (Hundred T)
-  Tr: 1000000000000, // 10^12 (Trillion)
-  HTr: 100000000000, // 10^11 (Hundred B)
-  B: 1000000000, // 10^9
-  M: 1000000,
-  K: 1000,
-  QDecimals: 18,
-  TDecimals: 15,
-  HDecimals: 14,
-  TrDecimals: 12,
-  HTrDecimals: 11,
-  BDecimals: 9,
-  MDecimals: 6,
-  KDecimals: 3,
-  DecimalsAfterDot: 2
-}
-
 export const formatNumber = (
   number: number | bigint | string,
   noDecimals?: boolean,
