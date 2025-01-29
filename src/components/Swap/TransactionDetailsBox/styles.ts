624213243cd7a0747282abe507f9b22e362f2db1
jakubkowolik <jakubkowolik00@gmail.com>
2025-01-29 11:14:31 +0100

    styles fix

--- src/components/Swap/TransactionDetailsBox/styles.ts
+++ src/components/Swap/TransactionDetailsBox/styles.ts
@@ -38,36 +38,5 @@ export const useStyles = makeStyles<{ open: boolean }>()((_theme, { open }) => (
   loading: {
     width: 15,
     height: 15
-  },
-  swapFlowContainer: {
-    maxHeight: 70,
-    margin: '1px',
-    gap: 16,
-    padding: '12px 30px 12px 30px',
-    paddingInline: '',
-    background: colors.invariant.component,
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10
-  },
-  routeIcon: {
-    width: 80
-  },
-  arrowContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 5,
-    alignItems: 'center'
-  },
-  routeLabel: {
-    ...typography.tiny2,
-    color: colors.invariant.text
-  },
-  tokenLabel: {
-    ...typography.body1,
-    color: colors.invariant.textGrey
-  },
-  tokenContinaer: {
-    display: 'flex',
-    flexDirection: 'column'
   }
 }))
