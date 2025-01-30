4356e9e8ba7855fab0d3f35395cb65f05018e5c0
Jakub Wołodko <kuba8369@gmail.com>
2025-01-30 20:45:37 +0100

    Revert "Revert "Refactor""
    This reverts commit e95e85ee6f5ce3430f398f89d30aae82c165ea5a.

---
+++ src/components/OverviewYourPositions/hooks/useLiquidity.ts
@@ -0,0 +1,49 @@
+import { calculatePriceSqrt } from '@invariant-labs/sdk-eclipse'
+import { getX, getY } from '@invariant-labs/sdk-eclipse/lib/math'
+import { printBN } from '@utils/utils'
+import { useMemo } from 'react'
+
+export const useLiquidity = (position: any) => {
+  const tokenXLiquidity = useMemo(() => {
+    if (position) {
+      try {
+        return +printBN(
+          getX(
+            position.liquidity,
+            calculatePriceSqrt(position.upperTickIndex),
+            position.poolData.sqrtPrice,
+            calculatePriceSqrt(position.lowerTickIndex)
+          ),
+          position.tokenX.decimals
+        )
+      } catch (error) {
+        return 0
+      }
+    }
+
+    return 0
+  }, [position])
+
+  const tokenYLiquidity = useMemo(() => {
+    if (position) {
+      try {
+        return +printBN(
+          getY(
+            position.liquidity,
+            calculatePriceSqrt(position.upperTickIndex),
+            position.poolData.sqrtPrice,
+            calculatePriceSqrt(position.lowerTickIndex)
+          ),
+          position.tokenY.decimals
+        )
+      } catch (error) {
+        console.log(error)
+        return 0
+      }
+    }
+
+    return 0
+  }, [position])
+
+  return { tokenXLiquidity, tokenYLiquidity }
+}
