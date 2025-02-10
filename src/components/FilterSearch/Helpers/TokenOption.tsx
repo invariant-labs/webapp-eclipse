e7f466d2b0b7ea9ec8443fd7b858e3cf6a2bb420
jakubkowolik <jakubkowolik00@gmail.com>
2025-02-10 14:42:32 +0100

    SelectTokenModal styles deletion-fix

--- src/components/FilterSearch/Helpers/TokenOption.tsx
+++ src/components/FilterSearch/Helpers/TokenOption.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import icons from '@static/icons'
 import { shortenAddress } from '@utils/uiUtils'
 import useStyles from './style'
