ecac98351fe293cbca34b5a2d6f9f2a4d312cf61
jakubkowolik <jakubkowolik00@gmail.com>
2025-02-12 15:43:23 +0100

    pagination fix

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -23,20 +23,18 @@ export const PaginationList: React.FC<IPaginationList> = ({
   const position = useMediaQuery(theme.breakpoints.down('sm'))
   const matches = useMediaQuery(theme.breakpoints.down('xs'))
   return (
-    <div
-      className={classes.root}
+    <Pagination
       style={{
         justifyContent: position ? 'center' : `${variant}`,
         alignItems: 'center'
-      }}>
-      <Pagination
-        count={pages}
-        shape='rounded'
-        defaultPage={defaultPage}
-        onChange={(_e, page) => handleChangePage(page)}
-        siblingCount={squeeze ? 0 : matches ? 0 : 1}
-        page={page}
-      />
-    </div>
+      }}
+      className={classes.root}
+      count={pages}
+      shape='rounded'
+      defaultPage={defaultPage}
+      onChange={(_e, page) => handleChangePage(page)}
+      siblingCount={squeeze ? 0 : matches ? 0 : 1}
+      page={page}
+    />
   )
 }
