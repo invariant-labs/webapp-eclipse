a841e44d841e725c9d9e9c308f0375205bee8507
Jakub Wołodko <kuba8369@gmail.com>
2025-02-13 23:15:48 +0100

    Fix

--- src/components/EmptyPlaceholder/style.ts
+++ src/components/EmptyPlaceholder/style.ts
@@ -1,74 +1,59 @@
 import { colors, typography } from '@static/theme'
 import { makeStyles } from 'tss-react/mui'
 
-interface StyleProps {
-  newVersion?: boolean
-  height?: string
-  blurWidth?: string
-  roundedCorners?: boolean
-}
+export const useStyles = makeStyles()(() => ({
+  container: {
+    width: '100%',
+    height: '370px',
+    zIndex: 14
+  },
+  root: {
+    zIndex: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 25,
+    width: '100%',
+    height: '100%',
+    p: {
+      textAlign: 'center'
+    }
+  },
+  img: {
+    paddingBottom: 25
+  },
+  blur: {
+    width: '100%',
+    height: '370px',
+    backgroundColor: 'rgba(12, 11, 13, 0.8)',
+    position: 'absolute',
+    zIndex: 13,
+    borderRadius: 10
+  },
+  desc: {
+    ...typography.body2,
+    fontWeight: 500,
+    lineHeight: '20px',
+    color: colors.invariant.lightHover
+  },
+  button: {
+    height: 40,
+    width: 200,
+    marginTop: 20,
+    color: colors.invariant.componentBcg,
+    ...typography.body1,
+    textTransform: 'none',
+    borderRadius: 14,
+    background: colors.invariant.pinkLinearGradientOpacity,
 
-export const useStyles = makeStyles<StyleProps>()(
-  (theme, { newVersion, roundedCorners, height, blurWidth }) => ({
-    container: {
-      width: '100%',
-      height: height ? height : '370px',
-      zIndex: 14
-    },
-    root: {
-      zIndex: 10,
-      display: 'flex',
-
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingBottom: 25,
-      width: '100%',
-      height: '100%',
-      p: {
-        textAlign: 'center'
-      }
-    },
-    img: {
-      paddingBottom: 25
-    },
-    blur: {
-      width: blurWidth ? blurWidth : '100%',
-      height: height ? height : '370px',
-      position: 'absolute',
-      transform: newVersion ? 'translate(-50%, -50%)' : 'unset',
-      top: newVersion ? '50%' : 0,
-      left: newVersion ? '50%' : 0,
-      zIndex: 13,
-      borderRadius: newVersion && !roundedCorners ? 0 : 10,
-      background: newVersion
-        ? 'linear-gradient(180deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%)'
-        : 'rgba(12, 11, 13, 0.8)'
-    },
-    desc: {
-      ...typography.body2,
-      fontWeight: 500,
-      lineHeight: '20px',
-      color: colors.invariant.lightHover
-    },
-    button: {
-      height: 40,
-      width: 200,
-      marginTop: 20,
-      color: colors.invariant.componentBcg,
-      ...typography.body1,
-      textTransform: 'none',
-      borderRadius: 14,
-      background: colors.invariant.pinkLinearGradientOpacity,
-
-      '&:hover': {
-        background: colors.invariant.pinkLinearGradient,
-        boxShadow: '0px 0px 16px rgba(239, 132, 245, 0.35)',
-        '@media (hover: none)': {
-          background: colors.invariant.pinkLinearGradientOpacity,
-          boxShadow: 'none'
-        }
+    '&:hover': {
+      background: colors.invariant.pinkLinearGradient,
+      boxShadow: '0px 0px 16px rgba(239, 132, 245, 0.35)',
+      '@media (hover: none)': {
+        background: colors.invariant.pinkLinearGradientOpacity,
+        boxShadow: 'none'
       }
     }
-  })
-)
+  }
+}))
