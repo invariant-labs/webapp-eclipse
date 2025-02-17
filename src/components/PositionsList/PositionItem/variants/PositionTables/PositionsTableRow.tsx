1d239fad7a0e699d78dfcad34522a4d3fa2ead31
Jakub Wołodko <kuba8369@gmail.com>
2025-02-17 13:23:21 +0100

    Update

--- src/components/PositionsList/PositionItem/variants/PositionTables/PositionsTableRow.tsx
+++ src/components/PositionsList/PositionItem/variants/PositionTables/PositionsTableRow.tsx
@@ -76,16 +76,16 @@ export const PositionTableRow: React.FC<IPositionsTableRow> = ({
 
   const isDesktop = useMediaQuery(theme.breakpoints.up('lg'))
 
-  const { tokenValueInUsd, tokenXPercentage, tokenYPercentage, unclaimedFeesInUSD } =
-    useUnclaimedFee({
-      currentPrice,
-      id,
-      position,
-      tokenXLiq,
-      tokenYLiq,
-      positionSingleData,
-      xToY
-    })
+  // const { tokenValueInUsd, tokenXPercentage, tokenYPercentage, unclaimedFeesInUSD } =
+  //   useUnclaimedFee({
+  //     currentPrice,
+  //     id,
+  //     position,
+  //     tokenXLiq,
+  //     tokenYLiq,
+  //     positionSingleData,
+  //     xToY
+  //   })
 
   const { isPromoted, pointsPerSecond, estimated24hPoints } = usePromotedPool(
     poolAddress,
@@ -196,13 +196,11 @@ export const PositionTableRow: React.FC<IPositionsTableRow> = ({
         alignItems='center'
         wrap='nowrap'>
         <Grid className={sharedClasses.infoCenter} container item justifyContent='center'>
-          <Typography className={sharedClasses.greenText}>
-            {`$${formatNumber2(tokenValueInUsd.value)}`}
-          </Typography>
+          <Typography className={sharedClasses.greenText}>{`$${formatNumber2(0)}`}</Typography>
         </Grid>
       </Grid>
     ),
-    [tokenValueInUsd, valueX, valueY, tokenXName, classes, isXs, isDesktop, tokenYName, xToY]
+    [valueX, valueY, tokenXName, classes, isXs, isDesktop, tokenYName, xToY]
   )
 
   const unclaimedFee = useMemo(
@@ -215,13 +213,11 @@ export const PositionTableRow: React.FC<IPositionsTableRow> = ({
         alignItems='center'
         wrap='nowrap'>
         <Grid className={sharedClasses.infoCenter} container item justifyContent='center'>
-          <Typography className={sharedClasses.greenText}>
-            ${formatNumber2(unclaimedFeesInUSD.value)}
-          </Typography>
+          <Typography className={sharedClasses.greenText}>${formatNumber2(0)}</Typography>
         </Grid>
       </Grid>
     ),
-    [unclaimedFeesInUSD, valueX, valueY, tokenXName, classes, isXs, isDesktop, tokenYName, xToY]
+    [valueX, valueY, tokenXName, classes, isXs, isDesktop, tokenYName, xToY]
   )
 
   const promotedIconContent = useMemo(() => {
@@ -388,7 +384,7 @@ export const PositionTableRow: React.FC<IPositionsTableRow> = ({
             padding: '8px 12px',
             borderRadius: '12px'
           }}>
-          {tokenXPercentage === 100 && (
+          {/* {tokenXPercentage === 100 && (
             <span>
               {tokenXPercentage}
               {'%'} {xToY ? tokenXName : tokenYName}
@@ -407,14 +403,11 @@ export const PositionTableRow: React.FC<IPositionsTableRow> = ({
               {'%'} {xToY ? tokenXName : tokenYName} {' - '} {tokenYPercentage}
               {'%'} {xToY ? tokenYName : tokenXName}
             </span>
-          )}
+          )} */}
         </Typography>
       </TableCell>
 
-      {/*
-       */}
-
-      {tokenValueInUsd.loading ? (
+      {false ? (
         <TableCell className={`${classes.cellBase} ${classes.valueCell}`}>
           <Skeleton
             variant='rectangular'
@@ -428,7 +421,7 @@ export const PositionTableRow: React.FC<IPositionsTableRow> = ({
           {valueFragment}
         </TableCell>
       )}
-      {unclaimedFeesInUSD.loading ? (
+      {false ? (
         <TableCell className={`${classes.cellBase} ${classes.feeCell}`}>
           <Skeleton
             variant='rectangular'
