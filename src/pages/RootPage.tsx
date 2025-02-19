190954d241ab132c06aa3fae6cac687d5d550278
matepal00 <mateusz.paluch2311@gmail.com>
2025-02-19 12:40:33 +0100

    change wallet in extension

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback, memo, useState, useLayoutEffect } from 'react'
+import { useEffect, useCallback, memo, useState, useLayoutEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useLocation, Outlet } from 'react-router-dom'
 import EventsHandlers from '@containers/EventsHandlers'
@@ -11,11 +11,14 @@ import { toBlur } from '@utils/uiUtils'
 import useStyles from './style'
 import { status } from '@store/selectors/solanaWallet'
 import { Status as WalletStatus } from '@store/reducers/solanaWallet'
+import { actions as walletActions } from '@store/reducers/solanaWallet'
+import { actions as leaderboardActions } from '@store/reducers/leaderboard'
 import { actions } from '@store/reducers/positions'
-
-import { NetworkType } from '@store/consts/static'
+import { DEFAULT_PUBLICKEY, NetworkType } from '@store/consts/static'
 import { network } from '@store/selectors/solanaConnection'
 import { NormalBanner } from '@components/Leaderboard/LeaderboardBanner/NormalBanner'
+import { getEclipseWallet } from '@utils/web3/wallet'
+import { leaderboardSelectors } from '@store/selectors/leaderboard'
 
 const BANNER_STORAGE_KEY = 'invariant-banner-state-3'
 const BANNER_HIDE_DURATION = 1000 * 60 * 60 * 24 // 24 hours
@@ -75,6 +78,42 @@ const RootPage: React.FC = memo(() => {
   useEffect(() => {
     initConnection()
   }, [initConnection])
+
+  const walletAddressRef = useRef('')
+  const itemsPerPage = useSelector(leaderboardSelectors.itemsPerPage)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const addr = getEclipseWallet().publicKey.toString()
+      if (
+        !walletAddressRef.current ||
+        (walletAddressRef.current === DEFAULT_PUBLICKEY.toString() &&
+          addr !== DEFAULT_PUBLICKEY.toString())
+      ) {
+        walletAddressRef.current = addr
+        return
+      }
+
+      if (
+        !document.hasFocus() &&
+        walletAddressRef.current !== DEFAULT_PUBLICKEY.toString() &&
+        walletAddressRef.current !== addr
+      ) {
+        walletAddressRef.current = addr
+        dispatch(walletActions.changeWalletInExtension())
+        dispatch(leaderboardActions.getLeaderboardData({ page: 1, itemsPerPage }))
+      }
+
+      if (
+        document.hasFocus() &&
+        walletAddressRef.current !== DEFAULT_PUBLICKEY.toString() &&
+        walletAddressRef.current !== addr
+      ) {
+        walletAddressRef.current = addr
+      }
+    }, 500)
+
+    return () => clearInterval(intervalId)
+  }, [])
 
   useEffect(() => {
     if (signerStatus === Status.Initialized && walletStatus === WalletStatus.Initialized) {
