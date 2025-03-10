bf7abee63ae8236efbe1c01dfadb515392a9b7f6
zielvna <zielvna@gmail.com>
2025-03-10 12:27:47 +0100

    fix placeholders

--- src/components/EmptyPlaceholder/style.ts
+++ src/components/EmptyPlaceholder/style.ts
@@ -99,6 +99,11 @@ export const useStyles = makeStyles<StyleProps>()(
     buttonText: {
       ...typography.body2
     },
-    button2: {}
+    button2: {},
+    buttonContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }
   })
 )
