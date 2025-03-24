c08aedd7cc3b3ec21a948ff4025da5a22b46bcc4
matepal00 <mateusz.paluch2311@gmail.com>
2025-03-24 09:35:34 +0100

    bump

--- src/components/Leaderboard/LeaderboardList/LeaderboardItem/LeaderboardLpItem.tsx
+++ src/components/Leaderboard/LeaderboardList/LeaderboardItem/LeaderboardLpItem.tsx
@@ -115,15 +115,12 @@ const LeaderboardLpItem: React.FC<LeaderboardLpItemProps> = props => {
         }}>
         <Typography style={{ color: getColorByPlace(rank) }}>{rank}</Typography>
 
-        <Typography style={{ paddingRight: '24px', width: 'auto' }}>
-          <span className={classes.address}>
-            {domain
-              ? isVerySmallScreen || isNarrowMediumScreen
-                ? shortDomain
-                : domain
-              : shortenAddress(address.toString(), 4)}{' '}
-          </span>
-
+        <Typography>
+          {domain
+            ? isVerySmallScreen || isNarrowMediumScreen
+              ? shortDomain
+              : domain
+            : shortenAddress(address.toString(), 4)}{' '}
           {isYou ? (
             <Typography style={{ color: colors.invariant.pink, marginLeft: '5px' }}>
               (You)
