85d6427e6ee90d2bc4e612a055ca3eb10eb431ab
Jakub Wołodko <kuba8369@gmail.com>
2025-03-28 10:48:01 +0100

    Fix default font in Slippage settings component

--- src/components/Modals/Slippage/style.ts
+++ src/components/Modals/Slippage/style.ts
@@ -86,6 +86,7 @@ export const useStyles = makeStyles()(() => {
       padding: 0,
       width: 49,
       height: 28,
+      fontFamily: 'Mukta',
       cursor: 'pointer',
       marginLeft: 8,
       ...typography.body2,
