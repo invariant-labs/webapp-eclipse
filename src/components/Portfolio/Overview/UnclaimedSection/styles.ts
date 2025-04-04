61152e0b5e5c0ed89096974226ac1fc87ebc2078
Jakub Wołodko <kuba8369@gmail.com>
2025-04-04 13:41:43 +0200

    Fix vertical jump

--- src/components/Portfolio/Overview/UnclaimedSection/styles.ts
+++ src/components/Portfolio/Overview/UnclaimedSection/styles.ts
@@ -14,6 +14,7 @@ export const useStyles = makeStyles<{ isLoading: boolean }>()((_theme: Theme, { 
     flexDirection: 'column',
     gap: '16px',
     minHeight: '32px',
+    maxHeight: '76px',
 
     [theme.breakpoints.up('lg')]: {
       height: '57.5px',
