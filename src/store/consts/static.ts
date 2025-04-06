b380eb6dbc2c964c3fb079733823f0770b56a3cf
Aleksander <dygons14@gmail.com>
2025-04-06 14:31:06 +0200

    Increase snap time delay

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -782,7 +782,7 @@ export type LeaderBoardType = 'Liquidity' | 'Swap' | 'Total'
 
 export const BANNER_STORAGE_KEY = 'invariant-warning-banner'
 export const BANNER_HIDE_DURATION = 1000 * 60 * 60 * 1 // 1 hour
-export const SNAP_TIME_DELAY = 60 * 4 // IN MINUTES (4 hours)
+export const SNAP_TIME_DELAY = 60 * 14 // IN MINUTES (14 hours)
 
 export enum OverviewSwitcher {
   Overview = 'Overview',
