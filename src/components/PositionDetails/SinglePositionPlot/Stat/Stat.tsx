c7f0ccfe6cb36b50320d2fe459b80da337263b83
Jakub <jakubkowolik00@gmail.com>
2025-04-15 17:56:40 +0200

    loaders leftovers

--- src/components/PositionDetails/SinglePositionPlot/Stat/Stat.tsx
+++ src/components/PositionDetails/SinglePositionPlot/Stat/Stat.tsx
@@ -1,19 +1,29 @@
-import { Box } from '@mui/material'
+import { Box, Skeleton } from '@mui/material'
 import { useStyles } from './style'
 
 type Props = {
   name?: React.ReactNode
   value: React.ReactNode
   isHorizontal?: boolean
+  isLoading?: boolean
 }
 
-export const Stat = ({ name, value, isHorizontal = false }: Props) => {
+export const Stat = ({ isLoading = false, name, value, isHorizontal = false }: Props) => {
   const { classes } = useStyles({ isHorizontal })
 
   return (
     <Box className={classes.container}>
       {name && <Box className={classes.name}>{name}</Box>}
-      <Box className={classes.value}>{value}</Box>
+      {isLoading ? (
+        <Skeleton
+          sx={{ alignSelf: 'center', margin: 'auto' }}
+          variant='rounded'
+          width={100}
+          height={27}
+        />
+      ) : (
+        <Box className={classes.value}>{value}</Box>
+      )}
     </Box>
   )
 }
