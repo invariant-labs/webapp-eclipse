98bb2ac5e8d7ea98403c2bcd9c372f6c0b4bbb79
Piotr <piotr.matlak.contact@gmail.com>
2025-05-02 14:43:54 +0200

    [216] change format of min - max on position details

--- src/components/PositionDetails/SinglePositionPlot/SinglePositionPlot.tsx
+++ src/components/PositionDetails/SinglePositionPlot/SinglePositionPlot.tsx
@@ -4,7 +4,7 @@ import {
   calcPriceByTickIndex,
   calcTicksAmountInRange,
   calculateConcentration,
-  formatNumberWithSuffix,
+  formatNumberWithoutSuffix,
   numberToString,
   spacingMultiplicityGte,
   truncateString
@@ -260,7 +260,7 @@ const SinglePositionPlot: React.FC<ISinglePositionPlot> = ({
                 value={
                   <Box>
                     <Typography component='span' className={classes.value}>
-                      {isFullRange ? 0 : formatNumberWithSuffix(min)}
+                      {isFullRange ? 0 : formatNumberWithoutSuffix(min)}
                     </Typography>{' '}
                     {!isFullRange &&
                       (xToY
@@ -280,7 +280,7 @@ const SinglePositionPlot: React.FC<ISinglePositionPlot> = ({
                       {isFullRange ? (
                         <span style={{ fontSize: '24px' }}>∞</span>
                       ) : (
-                        formatNumberWithSuffix(max)
+                        formatNumberWithoutSuffix(max)
                       )}
                     </Typography>{' '}
                     {!isFullRange &&
