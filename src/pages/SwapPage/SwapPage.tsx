ff806af7d40a87906a5faea81da7d8b1831848ab
Piotr <piotr.matlak.contact@gmail.com>
2025-05-13 15:37:09 +0200

    fix build

--- src/pages/SwapPage/SwapPage.tsx
+++ src/pages/SwapPage/SwapPage.tsx
@@ -1,18 +1,35 @@
-import WrappedSwap from '@containers/WrappedSwap/WrappedSwap'
+// import WrappedSwap from '@containers/WrappedSwap/WrappedSwap'
 import useStyles from './styles'
-import { Grid } from '@mui/material'
-import { useParams } from 'react-router-dom'
+import { Grid, Typography } from '@mui/material'
+// import { useParams } from 'react-router-dom'
 
 export const SwapPage: React.FC = () => {
   const { classes } = useStyles()
-  const { item1, item2 } = useParams()
+  // const { item1, item2 } = useParams()
 
-  const initialTokenFrom = item1 || ''
-  const initialTokenTo = item2 || ''
+  // const initialTokenFrom = item1 || ''
+  // const initialTokenTo = item2 || ''
 
   return (
     <Grid className={classes.container}>
-      <WrappedSwap initialTokenFrom={initialTokenFrom} initialTokenTo={initialTokenTo} />
+      {/* <WrappedSwap initialTokenFrom={initialTokenFrom} initialTokenTo={initialTokenTo} /> */}
+      <div>
+        <Typography color={'white'} maxWidth={'100%'}>
+          {'solana provider -----' + (window as any).nightly?.solana
+            ? JSON.stringify((window as any).nightly?.solana, null, 1)
+            : 'solana not found'}
+        </Typography>
+        {/* <br />
+    <Typography color={'white'} maxWidth={'100%'}>
+      {'features  -----' + JSON.stringify((window as any).nightly?.solana?.features, null, 1)}
+    </Typography> */}
+        <br />
+        <Typography color={'white'} maxWidth={'100%'}>
+          {'_activeAccount    ---- ' + (window as any).nightly?.solana?._activeAccount
+            ? JSON.stringify((window as any).nightly?.solana?._activeAccount, null, 1)
+            : 'solana account not found'}
+        </Typography>
+      </div>
     </Grid>
   )
 }
