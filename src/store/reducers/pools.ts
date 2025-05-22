0d9bd82be11f6ca2f9d363fdfde56deeaded60f5
Jakub Wołodko <kuba8369@gmail.com>
2025-05-22 13:25:53 +0200

    temp enable testnet

--- src/store/reducers/pools.ts
+++ src/store/reducers/pools.ts
@@ -81,10 +81,10 @@ export interface UpdateAutoSwapTicksAndTickmap {
   ticks: Tick[]
 }
 const network =
-  process.env.REACT_APP_NODE_ENV === 'development'
-    ? NetworkType[localStorage.getItem('INVARIANT_NETWORK_ECLIPSE') as keyof typeof NetworkType] ??
-      NetworkType.Mainnet
-    : NetworkType.Mainnet
+  // process.env.REACT_APP_NODE_ENV === 'development'
+  NetworkType[localStorage.getItem('INVARIANT_NETWORK_ECLIPSE') as keyof typeof NetworkType] ??
+  NetworkType.Mainnet
+// : NetworkType.Mainnet
 
 export const defaultState: IPoolsStore = {
   tokens: { ...getNetworkTokensList(network) },
