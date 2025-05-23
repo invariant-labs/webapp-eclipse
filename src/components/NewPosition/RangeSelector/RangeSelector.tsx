5e73eedd09581acf7120f477f05d024e4354d3fd
Wojciech Cichocki <43304803+wojciech-cichocki@users.noreply.github.com>
2025-05-23 17:39:43 +0200

    Update src/components/NewPosition/RangeSelector/RangeSelector.tsx

--- src/components/NewPosition/RangeSelector/RangeSelector.tsx
+++ src/components/NewPosition/RangeSelector/RangeSelector.tsx
@@ -524,7 +524,7 @@ export const RangeSelector: React.FC<IRangeSelector> = ({
               <Box className={classes.priceWarningContainer}>
                 <img className={classes.priceWarningIcon} src={warning3} alt='warning icon' />
                 <Typography className={classes.priceWarning}>
-                  Price might differ from actual price
+                  The current pool's price might differ from the actual price
                 </Typography>
               </Box>
             )}
