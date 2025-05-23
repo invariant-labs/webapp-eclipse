2f39b6cadd8479d60af92b9e35b4be401b52e518
Piotr <piotr.matlak.contact@gmail.com>
2025-05-23 16:53:08 +0200

    remove snackbar errors

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1845,7 +1845,6 @@ export const addressToTicker = (network: NetworkType, address: string): string =
 
 export const initialXtoY = (tokenXAddress?: string | null, tokenYAddress?: string | null) => {
   if (!tokenXAddress || !tokenYAddress) {
-    console.log('revert other tokens')
     return true
   }
 
