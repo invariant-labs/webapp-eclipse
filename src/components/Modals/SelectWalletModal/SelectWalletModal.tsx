798688516c4bc3f1bef7ddf9e12ef3642b0ef98b
Piotr <piotr.matlak.contact@gmail.com>
2025-05-28 15:27:37 +0200

    handle connect wallet on mobile

--- src/components/Modals/SelectWalletModal/SelectWalletModal.tsx
+++ src/components/Modals/SelectWalletModal/SelectWalletModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Button, Grid, Popover, Typography, Divider, Box } from '@mui/material'
 import useStyles from './styles'
 import { walletNames } from '@store/consts/static'
@@ -8,11 +8,13 @@ import { changeToNightlyAdapter, connectStaticWallet } from '@utils/web3/wallet'
 import {
   backpackWalletIcon,
   closeSmallIcon,
+  connectWalletIcon,
   nightlyConnectIcon,
   nightlyIcon,
   okxLogoIcon
 } from '@static/icons'
 import salmonIcon from '@static/png/salmonLogo.png'
+import useIsMobile from '@store/hooks/isMobile'
 
 export interface ISelectWalletModal {
   open: boolean
@@ -32,12 +34,36 @@ export const SelectWalletModal: React.FC<ISelectWalletModal> = ({
   setIsOpenSelectWallet,
   onDisconnect
 }) => {
-  const { classes } = useStyles()
+  const detectedWallet = useMemo(() => {
+    const nightly = (window as any)?.nightly
+    const backpack = (window as any)?.backpack
+    const okx = (window as any)?.okx
+    const salmon = (window as any)?.salmon
+
+    if (nightly && !backpack && !okx && !salmon) {
+      return WalletType.NIGHTLY
+    } else if (backpack && !nightly && !okx && !salmon) {
+      return WalletType.BACKPACK
+    } else if (okx && !nightly && !backpack && !salmon) {
+      return WalletType.OKX
+    } else if (salmon && !nightly && !backpack && !okx) {
+      return WalletType.SALMON
+    } else {
+      return null
+    }
+  }, [
+    (window as any).nightly,
+    (window as any).backpack,
+    (window as any).okx,
+    (window as any).salmon
+  ])
+
+  const isMobile = useIsMobile(true)
+  const { classes } = useStyles({ isMobile: isMobile && detectedWallet !== null })
 
   const setWallet = (wallet: WalletType) => {
     localStorage.setItem('WALLET_TYPE', wallet.toString())
   }
-
   const handleConnectStaticWallet = async (wallet: WalletType) => {
     setIsOpenSelectWallet(false)
     setTimeout(async () => {
@@ -45,12 +71,24 @@ export const SelectWalletModal: React.FC<ISelectWalletModal> = ({
         await (async () => onDisconnect())()
         await new Promise(resolve => setTimeout(resolve, 300))
       }
-
       await connectStaticWallet(wallet)
       handleConnect()
     }, 300)
   }
 
+  const handleNightlySelector = () => {
+    setIsOpenSelectWallet(false)
+    setTimeout(async () => {
+      if (isChangeWallet) {
+        await (async () => onDisconnect())()
+        await new Promise(resolve => setTimeout(resolve, 100))
+      }
+      changeToNightlyAdapter()
+      await openWalletSelectorModal()
+      handleConnect()
+      setWallet(WalletType.NIGHTLY)
+    }, 300)
+  }
   return (
     <div className={classes.modalContainer}>
       <Popover
@@ -62,128 +100,154 @@ export const SelectWalletModal: React.FC<ISelectWalletModal> = ({
         }}
         onClose={handleClose}>
         <Box className={classes.root}>
-          <Grid
-            className={classes.topCloseButton}
-            onClick={() => {
-              setIsOpenSelectWallet(false)
-            }}>
-            <img width={16} src={closeSmallIcon} alt='Close'></img>
-          </Grid>
-          <Typography className={classes.title}>Connect your wallet</Typography>
-          <Grid className={classes.buttonWrapper}>
-            <Typography className={classes.subTitle}>
-              Connect using Nightly's auto-detection
-            </Typography>
-            <Grid className={classes.buttonList}>
-              <Grid
-                item
-                className={classes.button}
-                onClick={() => {
-                  setIsOpenSelectWallet(false)
-                  setTimeout(async () => {
-                    if (isChangeWallet) {
-                      await (async () => onDisconnect())()
-                      await new Promise(resolve => setTimeout(resolve, 100))
-                    }
-                    changeToNightlyAdapter()
-                    await openWalletSelectorModal()
-                    handleConnect()
-                    setWallet(WalletType.NIGHTLY)
-                  }, 300)
+          <Box
+            display='flex'
+            justifyContent='space-between'
+            alignItems={isMobile && detectedWallet !== null ? 'center' : 'end'}
+            mb={isMobile && detectedWallet !== null ? 3 : 0}>
+            {(!isMobile || detectedWallet === null) && <Box width={16} />}
+            <Typography className={classes.title}>Connect your wallet</Typography>
+
+            <Grid
+              className={classes.topCloseButton}
+              onClick={() => {
+                setIsOpenSelectWallet(false)
+              }}>
+              <img width={16} src={closeSmallIcon} alt='Close'></img>
+            </Grid>
+          </Box>
+          {detectedWallet && isMobile ? (
+            <>
+              <Typography className={classes.mobileSubtitle}>
+                Connect your wallet to interact with Invariant!
+              </Typography>
+              <Box
+                className={classes.mobileWallet}
+                onClick={async () => {
+                  if (detectedWallet === WalletType.NIGHTLY) {
+                    console.log('test')
+                    handleNightlySelector()
+                  } else {
+                    handleConnectStaticWallet(detectedWallet)
+                  }
                 }}>
-                <Typography className={classes.buttonName}>
-                  <img
-                    width={53}
-                    rel='preload'
-                    src={nightlyConnectIcon}
-                    alt='nightly connect logo'></img>
-                  {walletNames[WalletType.NIGHTLY]}
+                <img src={connectWalletIcon} alt='Connect wallet' />
+                <Typography>Connect</Typography>
+              </Box>
+            </>
+          ) : (
+            <>
+              <Grid className={classes.buttonWrapper}>
+                <Typography className={classes.subTitle}>
+                  Connect using Nightly's auto-detection
                 </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Divider className={classes.divider} />
-
-          <Grid className={classes.buttonWrapper}>
-            <Typography className={classes.subTitle}>Or connect with popular wallets </Typography>
-            <Grid className={classes.buttonList}>
-              <Grid
-                item
-                className={classes.button}
-                onClick={async () => {
-                  handleConnectStaticWallet(WalletType.BACKPACK)
-                }}>
-                <Grid className={classes.buttonContainer}>
-                  <Typography className={classes.buttonName}>
-                    <img
-                      width={45}
-                      rel='preload'
-                      src={backpackWalletIcon}
-                      alt='backpack wallet icon'></img>
-
-                    {walletNames[WalletType.BACKPACK]}
-                  </Typography>
+                <Grid className={classes.buttonList}>
+                  <Grid
+                    item
+                    className={classes.button}
+                    onClick={() => {
+                      handleNightlySelector()
+                    }}>
+                    <Typography className={classes.buttonName}>
+                      <img
+                        width={53}
+                        rel='preload'
+                        src={nightlyConnectIcon}
+                        alt='nightly connect logo'></img>
+                      {walletNames[WalletType.NIGHTLY]}
+                    </Typography>
+                  </Grid>
                 </Grid>
               </Grid>
-              <Grid
-                item
-                className={classes.button}
-                onClick={async () => {
-                  handleConnectStaticWallet(WalletType.NIGHTLY_WALLET)
-                }}>
-                <Grid className={classes.buttonContainer}>
-                  <Typography className={classes.buttonName}>
-                    <img width={45} rel='preload' src={nightlyIcon} alt='nightly wallet icon'></img>
+              <Divider className={classes.divider} />
+              <Grid className={classes.buttonWrapper}>
+                <Typography className={classes.subTitle}>
+                  Or connect with popular wallets{' '}
+                </Typography>
+                <Grid className={classes.buttonList}>
+                  <Grid
+                    item
+                    className={classes.button}
+                    onClick={async () => {
+                      handleConnectStaticWallet(WalletType.BACKPACK)
+                    }}>
+                    <Grid className={classes.buttonContainer}>
+                      <Typography className={classes.buttonName}>
+                        <img
+                          width={45}
+                          rel='preload'
+                          src={backpackWalletIcon}
+                          alt='backpack wallet icon'></img>
 
-                    {walletNames[WalletType.NIGHTLY_WALLET]}
-                  </Typography>
+                        {walletNames[WalletType.BACKPACK]}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    item
+                    className={classes.button}
+                    onClick={async () => {
+                      handleConnectStaticWallet(WalletType.NIGHTLY_WALLET)
+                    }}>
+                    <Grid className={classes.buttonContainer}>
+                      <Typography className={classes.buttonName}>
+                        <img
+                          width={45}
+                          rel='preload'
+                          src={nightlyIcon}
+                          alt='nightly wallet icon'></img>
+
+                        {walletNames[WalletType.NIGHTLY_WALLET]}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    item
+                    className={classes.button}
+                    onClick={async () => {
+                      handleConnectStaticWallet(WalletType.OKX)
+                    }}>
+                    <Grid className={classes.buttonContainer}>
+                      <Typography className={classes.buttonName}>
+                        <img width={45} rel='preload' src={okxLogoIcon} alt='Close'></img>
+                        {walletNames[WalletType.OKX]}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    item
+                    className={classes.button}
+                    onClick={async () => {
+                      handleConnectStaticWallet(WalletType.SALMON)
+                    }}>
+                    <Grid className={classes.buttonContainer}>
+                      <Typography className={classes.buttonName}>
+                        <img
+                          width={45}
+                          rel='preload'
+                          src={salmonIcon}
+                          alt='salmon wallet icon'></img>
+
+                        {walletNames[WalletType.SALMON]}
+                      </Typography>
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
-              <Grid
-                item
-                className={classes.button}
-                onClick={async () => {
-                  handleConnectStaticWallet(WalletType.OKX)
-                }}>
-                <Grid className={classes.buttonContainer}>
-                  <Typography className={classes.buttonName}>
-                    <img width={45} rel='preload' src={okxLogoIcon} alt='Close'></img>
-                    {walletNames[WalletType.OKX]}
-                  </Typography>
-                </Grid>
+              <Divider className={classes.divider} />
+              <Grid className={classes.modalFooter}>
+                <Typography className={classes.footerTitle}>Don't have a wallet?</Typography>
+                <a href={' https://nightly.app/'} target='_blank'>
+                  <Button className={classes.buttonPrimary} variant='contained'>
+                    Download one!
+                  </Button>
+                </a>
               </Grid>
-              <Grid
-                item
-                className={classes.button}
-                onClick={async () => {
-                  handleConnectStaticWallet(WalletType.SALMON)
-                }}>
-                <Grid className={classes.buttonContainer}>
-                  <Typography className={classes.buttonName}>
-                    <img width={45} rel='preload' src={salmonIcon} alt='salmon wallet icon'></img>
-
-                    {walletNames[WalletType.SALMON]}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Divider className={classes.divider} />
-
-          <Grid className={classes.modalFooter}>
-            <Typography className={classes.footerTitle}>Don't have a wallet?</Typography>
-            <a href={' https://nightly.app/'} target='_blank'>
-              <Button className={classes.buttonPrimary} variant='contained'>
-                Download one!
-              </Button>
-            </a>
-          </Grid>
+            </>
+          )}
         </Box>
       </Popover>
     </div>
   )
 }
-
 export default SelectWalletModal
