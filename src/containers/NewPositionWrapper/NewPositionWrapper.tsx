80416fc397047b8e0fd80676639080e1bfe45e22
Piotr <piotr.matlak.contact@gmail.com>
2025-06-05 10:55:20 +0200

    hide promoted fee tier for one selected token

--- src/containers/NewPositionWrapper/NewPositionWrapper.tsx
+++ src/containers/NewPositionWrapper/NewPositionWrapper.tsx
@@ -778,6 +778,9 @@ export const NewPositionWrapper: React.FC<IProps> = ({
   }, [])
 
   const { feeTiersWithTvl, totalTvl } = useMemo(() => {
+    if (tokenAIndex === null || tokenBIndex === null) {
+      return { feeTiersWithTvl: {}, totalTvl: 0 }
+    }
     const feeTiersWithTvl: Record<number, number> = {}
     let totalTvl = 0
 
