8dbc37a0959cf95a08eb8592dec5e139fffaa75e
Jakub <jakubkowolik00@gmail.com>
2025-06-23 15:11:51 +0200

    Bold added

--- src/components/Stake/FAQSection/FAQSection.tsx
+++ src/components/Stake/FAQSection/FAQSection.tsx
@@ -8,57 +8,114 @@ const faqData = [
     answer: (
       <Typography sx={{ zIndex: 5, position: 'relative' }}>
         sBITZ is a liquid staking token you receive when you stake BITZ. It represents your staked
-        position and automatically increases in value as staking rewards accumulate. Instead of
-        claiming rewards manually once or twice a day, Invariant introduced an auto-compounding
-        mechanism that compounds staking rewards every ten seconds. This increases the base amount
-        from which rewards are calculated, allowing you to achieve a significantly higher APY
-        compared to traditional BITZ staking. In addition, you can use sBITZ in DeFi protocols (for
-        example, by providing liquidity) to earn extra yield while still continuously earning
-        staking rewards.
+        position and <b style={{ color: colors.invariant.green }}>automatically increases</b> in
+        value as staking rewards accumulate. Instead of claiming rewards manually once or twice a
+        day, Invariant introduced an{' '}
+        <b style={{ color: colors.invariant.green }}>auto-compounding</b> mechanism that{' '}
+        <b style={{ color: colors.invariant.green }}>compounds</b> staking rewards every ten
+        seconds. This increases the base amount from which rewards are calculated, allowing you to
+        achieve a significantly <b style={{ color: colors.invariant.green }}>higher APY </b>{' '}
+        compared to traditional BITZ staking. <br /> <br /> In addition, you can use sBITZ in DeFi
+        protocols (for example, by providing liquidity) to earn extra yield while still continuously
+        earning staking rewards.
       </Typography>
     )
   },
   {
     question: 'Why did Invariant create sBITZ?',
-    answer: `To solve the liquidity problem for BITZ and offer both current and future members of the Eclipse community more ways to earn from their capital, while also creating another incentive to bring TVL to Eclipse.
-Invariant has been contributing to the growth of DeFi on Eclipse from the very beginning, and this is another major step in that direction.
-`
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        To solve the liquidity problem for BITZ and offer both current and future members of the
+        Eclipse community more ways to earn from their capital, while also creating another
+        incentive to bring <b style={{ color: colors.invariant.green }}> TVL</b> to Eclipse.
+        Invariant has been contributing to the growth of DeFi on Eclipse from the very beginning,
+        and this is another major step in that direction.
+      </Typography>
+    )
   },
   {
     question: 'What are the benefits of having sBITZ?',
-    answer: `For you, it’s a way to earn additional yield (liquidity fees, Invariant Points) while still earning BITZ staking rewards.
-For Eclipse DeFi, it’s a solution to the liquidity problem for BITZ.`
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        For you, it’s a way to earn additional yield{' '}
+        <b style={{ color: colors.invariant.green }}>(liquidity fees, Invariant Points)</b> while
+        still earning BITZ staking rewards. For Eclipse DeFi, it’s a solution to the{' '}
+        <b style={{ color: colors.invariant.green }}>liquidity</b> problem for BITZ.
+      </Typography>
+    )
   },
   {
     question: 'How can I get sBITZ?',
-    answer:
-      'You can get sBITZ by staking your BITZ using the interface above or simply by swapping BITZ → sBITZ.'
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        You can get <b style={{ color: colors.invariant.green }}>sBITZ</b> by staking your{' '}
+        <b style={{ color: colors.invariant.green }}>BITZ</b> using the interface above or simply by
+        swapping <b style={{ color: colors.invariant.green }}>BITZ → sBITZ.</b>
+      </Typography>
+    )
   },
   {
     question: 'Are there any fees on sBITZ?',
-    answer: `No, Invariant doesn't charge any fees for minting or burning sBITZ. Using it comes with no additional costs other than standard network fees.`
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        No, Invariant doesn't charge <b style={{ color: colors.invariant.green }}>any fees</b> for
+        minting or burning sBITZ. Using it comes with{' '}
+        <b style={{ color: colors.invariant.green }}>no additional costs</b> other than standard
+        network fees.
+      </Typography>
+    )
   },
 
   {
     question: 'What is auto compounding and how does it work?',
-    answer: `Auto Compounding is a feature that automatically reinvests your staking rewards back into your sBITZ balance every 10 seconds. This increases your staked amount and boosts future rewards, creating a continuous compounding loop.
-That’s 8,640 times per day. The result is a much higher APY and more yield for you over time.`
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        Auto Compounding is a feature that automatically reinvests your staking rewards back into
+        your sBITZ balance every <b style={{ color: colors.invariant.green }}>10 seconds.</b> This
+        increases your staked amount and boosts future rewards, creating a continuous compounding
+        loop. That’s <b style={{ color: colors.invariant.green }}>8,640</b> times per day. The
+        result is a much <b style={{ color: colors.invariant.green }}>higher APY</b> and{' '}
+        <b style={{ color: colors.invariant.green }}>more yield</b> for you over time.
+      </Typography>
+    )
   },
 
   {
     question: 'What can I do with sBITZ?',
-    answer: `At this point, you can put your sBITZ to work by adding it to one of the available liquidity pools. This way, on top of earning staking rewards, you’ll also earn trading fees and Invariant Points.
-More yield opportunities on other protocols within the Eclipse DeFi ecosystem should be available soon.`
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        At this point, you can put your sBITZ to work by adding it to one of the available liquidity
+        pools. This way, on top of earning
+        <b style={{ color: colors.invariant.green }}> staking rewards</b>, you’ll also earn{' '}
+        <b style={{ color: colors.invariant.green }}>trading fees</b> and{' '}
+        <b style={{ color: colors.invariant.green }}>Invariant Points.</b> More yield opportunities
+        on other protocols within the Eclipse DeFi ecosystem should be available soon.
+      </Typography>
+    )
   },
   {
     question: 'How does price appreciation work for sBITZ?',
-    answer: `The value of sBITZ increases over time relative to BITZ. Instead of receiving separate staking rewards, one unit of sBITZ gradually becomes worth more BITZ.
-For example, if 1 sBITZ equals 1 BITZ at the start, it might be worth 1.05 BITZ after some time. The yield compounds automatically and no action is required.`
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        The value of sBITZ increases over time relative to BITZ. Instead of receiving separate
+        staking rewards, one unit of sBITZ gradually becomes worth more BITZ. For example, if{' '}
+        <b style={{ color: colors.invariant.green }}>1 sBITZ</b> equals{' '}
+        <b style={{ color: colors.invariant.green }}>1 BITZ</b> at the start, it might be worth{' '}
+        <b style={{ color: colors.invariant.green }}>1.05 BITZ</b> after some time. The yield
+        <b style={{ color: colors.invariant.green }}> compounds automatically</b> and no action is
+        required.
+      </Typography>
+    )
   },
   {
     question: 'How can I unstake sBITZ?',
-    answer:
-      'The same way as with staking — either through the interface above or by swapping sBITZ to BITZ, or even to ETH.'
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        The same way as with staking — either through the{' '}
+        <b style={{ color: colors.invariant.green }}>interface above</b> or by swapping{' '}
+        <b style={{ color: colors.invariant.green }}>sBITZ to BITZ</b>, or even to ETH.
+      </Typography>
+    )
   },
   {
     question: 'Can I instantly unstake sBITZ anytime I want?',
@@ -66,8 +123,14 @@ For example, if 1 sBITZ equals 1 BITZ at the start, it might be worth 1.05 BITZ 
   },
   {
     question: 'What are the risks of holding sBITZ',
-    answer:
-      'The same as holding BITZ. Staking through Invariant is no different from staking on PowPow, except that you receive sBITZ in return. The staked BITZ goes to the same smart contract as when staking via PowPow.'
+    answer: (
+      <Typography sx={{ zIndex: 5, position: 'relative' }}>
+        The same as holding BITZ. Staking through Invariant is{' '}
+        <b style={{ color: colors.invariant.green }}>no different</b> from staking on PowPow, except
+        that you receive sBITZ in return. The staked BITZ goes to the same{' '}
+        <b style={{ color: colors.invariant.green }}>smart contract</b> as when staking via PowPow.
+      </Typography>
+    )
   }
 ]
 
