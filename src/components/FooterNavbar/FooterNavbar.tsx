5bb04a5e8e9e6db332a5365132b4cb0fe2aa3c00
Jakub <jakubkowolik00@gmail.com>
2025-06-24 12:29:53 +0200

    More Icon navbar added

--- src/components/FooterNavbar/FooterNavbar.tsx
+++ src/components/FooterNavbar/FooterNavbar.tsx
@@ -1,10 +1,13 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Popover, Typography } from '@mui/material'
 import useStyles from './style'
 import { Link, useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import {
   airdropIcon,
   liquidityIcon,
+  lqStakingIcon,
+  moreIcon,
+  saleIcon,
   statsIcon,
   swapArrowsIcon,
   tokenCreatorIcon,
@@ -14,28 +17,72 @@ import { NetworkType } from '@store/consts/static'
 import { useSelector } from 'react-redux'
 import { network } from '@store/selectors/solanaConnection'
 import { colors } from '@static/theme'
+import { blurContent, unblurContent } from '@utils/uiUtils'
+import { Separator } from '@common/Separator/Separator'
+
+interface INavLinks {
+  label: string
+  icon: string
+  width: number
+  isLink: boolean
+  url?: string
+  onClick?: (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void
+}
 
 export const FooterNavbar = () => {
   const typeOfNetwork = useSelector(network)
 
-  const links = [
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null)
+  const [isMorePopupOpen, setIsMorePopupOpen] = useState(false)
+
+  const moreOptions: INavLinks[] = [
+    {
+      label: 'Liquid staking',
+      icon: lqStakingIcon,
+      url: 'stake',
+      onClick: () => {
+        setIsMorePopupOpen(false)
+        setAnchorEl(null)
+        unblurContent()
+      },
+      isLink: true,
+      width: 25
+    },
+    {
+      label: 'Sale',
+      icon: saleIcon,
+      url: 'sale',
+      onClick: () => {
+        setIsMorePopupOpen(false)
+        setAnchorEl(null)
+        unblurContent()
+      },
+      isLink: true,
+      width: 25
+    }
+  ]
+
+  const links: INavLinks[] = [
     {
       label: 'Swap',
       icon: swapArrowsIcon,
       url: 'exchange',
-      width: 33
+      width: 33,
+      isLink: true
     },
     {
       label: 'Liquidity',
       icon: liquidityIcon,
       url: 'liquidity',
-      width: 20
+      width: 20,
+      isLink: true
     },
     {
       label: 'Portfolio',
       icon: walletIcon,
       url: 'portfolio',
-      width: 26
+      width: 26,
+      isLink: true
     },
 
     typeOfNetwork === NetworkType.Testnet
@@ -43,20 +90,38 @@ export const FooterNavbar = () => {
           label: 'Creator',
           icon: tokenCreatorIcon,
           url: 'creator',
-          width: 33
+          width: 33,
+          isLink: true
         }
       : {
           label: 'Points',
           icon: airdropIcon,
           url: 'points',
-          width: 26
+          width: 26,
+          isLink: true
         },
     {
       label: 'Stats',
       icon: statsIcon,
       url: 'statistics',
-      width: 30
-    }
+      width: 30,
+      isLink: true
+    },
+    ...(typeOfNetwork === NetworkType.Mainnet
+      ? [
+          {
+            label: 'More',
+            icon: moreIcon,
+            width: 25,
+            isLink: false,
+            onClick: (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+              setAnchorEl(e.currentTarget as HTMLButtonElement)
+              setIsMorePopupOpen(true)
+              blurContent()
+            }
+          }
+        ]
+      : [])
   ]
 
   const location = useLocation()
@@ -75,7 +140,14 @@ export const FooterNavbar = () => {
     portfolio: [/^portfolio\/*/, /^newPosition\/*/, /^position\/*/],
 
     ...(typeOfNetwork === NetworkType.Mainnet ? { leaderboard: [/^points\/*/] } : {}),
-    ...(typeOfNetwork === NetworkType.Testnet ? { creator: [/^creator\/*/] } : {})
+    ...(typeOfNetwork === NetworkType.Testnet ? { creator: [/^creator\/*/] } : {}),
+    ...(typeOfNetwork === NetworkType.Mainnet ? { more: [/^stake\/*/] } : {})
+  }
+
+  const isMoreOptionActive = () => {
+    return moreOptions.some(
+      option => option.url === activePath || (option.url && activePath.startsWith(option.url))
+    )
   }
 
   const [display, setDisplay] = useState(true)
@@ -88,55 +160,172 @@ export const FooterNavbar = () => {
 
     window.visualViewport!.addEventListener('resize', resizeHandler)
 
-    return () => window.visualViewport!.addEventListener('resize', resizeHandler)
+    return () => window.visualViewport!.removeEventListener('resize', resizeHandler)
   }, [])
 
-  return (
-    <Box
-      component='footer'
-      className={classes.navbar}
-      style={{ display: display ? 'flex' : 'none' }}>
-      {links.map(link => {
-        const active =
-          link.url === activePath ||
-          (!!otherRoutesToHighlight[link.url] &&
-            otherRoutesToHighlight[link.url].some(pathRegex => pathRegex.test(activePath)))
+  const handleClosePopup = () => {
+    setIsMorePopupOpen(false)
+    setAnchorEl(null)
+    unblurContent()
+  }
 
-        return (
-          <Link
-            key={`path-${link.url}`}
-            to={`/${link.url}`}
-            className={classes.navbox}
-            style={{
-              background: active ? colors.invariant.light : ''
-            }}
-            onClick={e => {
-              if (link.url === 'exchange' && activePath.startsWith('exchange')) {
-                e.preventDefault()
-                return
-              }
-              setActive(link.url)
-            }}>
-            {active && <Box className={classes.activeBox} />}
-            <img
-              src={link.icon}
-              width={link.width}
-              style={
-                active
-                  ? { filter: 'brightness(0) saturate(100%) invert(100%)' }
-                  : { filter: 'brightness(0) saturate(100%) invert(45%)' }
-              }
-              className={classes.navImg}
-              alt={link.label}
-            />
-            <Typography
-              sx={active ? { color: colors.white.main } : { color: colors.invariant.textGrey }}>
-              {link.label}
-            </Typography>
-          </Link>
-        )
-      })}
-    </Box>
+  return (
+    <>
+      <Box
+        component='footer'
+        className={classes.navbar}
+        style={{ display: display ? 'flex' : 'none' }}>
+        {links.map((link, index) => {
+          let active = false
+
+          if (link.label === 'More') {
+            active = isMoreOptionActive()
+          } else {
+            active =
+              link.url === activePath ||
+              (!!link.url &&
+                !!otherRoutesToHighlight[link.url] &&
+                otherRoutesToHighlight[link.url].some(pathRegex => pathRegex.test(activePath)))
+          }
+
+          if (link.isLink && link.url) {
+            return (
+              <Link
+                key={`path-${link.url}`}
+                to={`/${link.url}`}
+                className={classes.navbox}
+                style={{
+                  background: active ? colors.invariant.light : ''
+                }}
+                onClick={e => {
+                  if (link.url === 'exchange' && activePath.startsWith('exchange')) {
+                    e.preventDefault()
+                    return
+                  }
+                  setActive(link.url!)
+                  if (link.onClick) {
+                    link.onClick(e)
+                  }
+                }}>
+                {active && <Box className={classes.activeBox} />}
+                <img
+                  src={link.icon}
+                  width={link.width}
+                  style={
+                    active
+                      ? { filter: 'brightness(0) saturate(100%) invert(100%)' }
+                      : { filter: 'brightness(0) saturate(100%) invert(45%)' }
+                  }
+                  className={classes.navImg}
+                  alt={link.label}
+                />
+                <Typography
+                  sx={active ? { color: colors.white.main } : { color: colors.invariant.textGrey }}>
+                  {link.label}
+                </Typography>
+              </Link>
+            )
+          } else {
+            return (
+              <Box
+                key={`button-${index}`}
+                component='button'
+                className={classes.navbox}
+                onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                  if (link.onClick) {
+                    link.onClick(e)
+                  }
+                }}
+                style={{
+                  background: active ? colors.invariant.light : 'transparent',
+                  border: 'none',
+                  cursor: 'pointer'
+                }}>
+                {active && <Box className={classes.activeBox} />}
+                <img
+                  src={link.icon}
+                  width={link.width}
+                  style={
+                    active
+                      ? { filter: 'brightness(0) saturate(100%) invert(100%)' }
+                      : { filter: 'brightness(0) saturate(100%) invert(45%)' }
+                  }
+                  className={classes.navImg}
+                  alt={link.label}
+                />
+                <Typography
+                  sx={active ? { color: colors.white.main } : { color: colors.invariant.textGrey }}>
+                  {link.label}
+                </Typography>
+              </Box>
+            )
+          }
+        })}
+      </Box>
+
+      <Popover
+        classes={{ paper: classes.paper }}
+        open={isMorePopupOpen}
+        anchorEl={anchorEl}
+        onClose={handleClosePopup}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}>
+        <Box className={classes.root}>
+          {moreOptions.map((option, index) => {
+            const isOptionActive =
+              option.url === activePath || (option.url && activePath.startsWith(option.url))
+
+            return (
+              <>
+                <Link
+                  key={option.url}
+                  to={`/${option.url}`}
+                  style={{ textDecoration: 'none' }}
+                  onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+                    if (option.onClick) {
+                      option.onClick(e)
+                    }
+                  }}>
+                  <Box
+                    className={classes.moreLink}
+                    style={{
+                      background: isOptionActive ? colors.invariant.light : 'transparent'
+                    }}>
+                    <img
+                      src={option.icon}
+                      width={option.width}
+                      alt={option.label}
+                      style={
+                        isOptionActive
+                          ? { filter: 'brightness(0) saturate(100%) invert(100%)' }
+                          : {}
+                      }
+                    />
+                    <Typography
+                      sx={
+                        isOptionActive
+                          ? { color: colors.white.main }
+                          : { color: colors.invariant.textGrey }
+                      }>
+                      {option.label}
+                    </Typography>
+                  </Box>
+                </Link>
+                {index + 1 !== moreOptions.length && (
+                  <Separator margin='4px' isHorizontal color={colors.invariant.light} />
+                )}
+              </>
+            )
+          })}
+        </Box>
+      </Popover>
+    </>
   )
 }
 
