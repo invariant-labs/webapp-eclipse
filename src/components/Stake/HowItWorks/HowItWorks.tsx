760fd3dc1f976a0ed3c3e79ee47e8762cc7019db
zielvna <zielvna@gmail.com>
2025-06-25 17:33:41 +0200

    update how it works component max width

--- src/components/Stake/HowItWorks/HowItWorks.tsx
+++ src/components/Stake/HowItWorks/HowItWorks.tsx
@@ -12,12 +12,22 @@ export const HowItWorks = () => {
       <Box className={classes.innerContainer}>
         <img className={classes.image} src={howItWorksImage} alt='how it works' />
         <Box className={classes.stepsContainer}>
-          <Step number='01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a.</Step>
-          <Step number='02' hasHiglight>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a.
+          <Step number='01'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt gravida mi, vel
+            sollicitudin purus.
           </Step>
-          <Step number='03'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a.</Step>
-          <Step number='04'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Step>
+          <Step number='02' hasHiglight>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nibh libero, laoreet
+            vitae imperdiet a, vestibulum et odio. Curabitur iaculis.
+          </Step>
+          <Step number='03'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec tortor ipsum. Phasellus
+            sodales ipsum.
+          </Step>
+          <Step number='04'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nibh libero, laoreet
+            vitae imperdiet a, vestibulum et odio. Curabitur iaculis.
+          </Step>
         </Box>
       </Box>
     </Box>
