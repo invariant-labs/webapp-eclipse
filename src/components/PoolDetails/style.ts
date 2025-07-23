5b4d224185a9ac5b3b4b8684ee24f02bd4e1f5b1
Piotr <piotr.matlak.contact@gmail.com>
2025-07-23 14:43:58 +0200

    fix top margin

--- src/components/PoolDetails/style.ts
+++ src/components/PoolDetails/style.ts
@@ -3,7 +3,6 @@ import { makeStyles } from 'tss-react/mui'
 
 export const useStyles = makeStyles()(theme => ({
   wrapper: {
-    marginTop: 24,
     maxWidth: 1210,
     display: 'flex',
     flexDirection: 'column',
